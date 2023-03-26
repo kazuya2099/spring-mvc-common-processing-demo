2d0a80a2dcc14d3f70a6be67b4f44bbc35422ee0
mhama <mhama@test.com>
2023-03-26 15:53:13 +0900

    up

--- pure-spring-demo/src/test/java/com/example/GreetingServiceTest.java
+++ pure-spring-demo/src/test/java/com/example/GreetingServiceTest.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import com.example.component.service.GreetingService;
-import com.example.config.AppConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.example.component.service.GreetingService;
+import com.example.config.WebConfig;
+
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = AppConfig.class)
+@ContextConfiguration(classes = WebConfig.class)
 class GreetingServiceTest {
 
   @Autowired
